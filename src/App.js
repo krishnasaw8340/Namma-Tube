cd2211d0426f382b6f8e9c5b22f17b890d52edd7
Krishna Saw <krishnakrsaw8340@gmail.com>
2024-05-08 08:04:59 +0530

    component-included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import Header from './component/Header';
+import Body from './component/Body';
+import Footer from './component/Footer';
+import Sidebar from './component/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl font-medium text-red-500'>Krishna</h1>
+      <Headers/>
+      <Body/>
+      <Sidebar/>
+      <Footer/>
     </div>
   );
 }
